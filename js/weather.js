5bd333c05fd91e846063f03e7fc306f7d5d54ff4
kwolberg <kwolberg@umich.edu>
2020-12-01 18:01:29 -0500

    finished homework

--- js/weather.js
+++ js/weather.js
@@ -16,26 +16,27 @@ function gettingJSON(){
     }
 
     // Your code here.
-    console.log("Location is : " + location);
+    console.log("Location is: " + location);
 
     //set default temperature format if one isn't provided
     let format; 
 
     if (document.querySelector("#celcius").checked == true) {
-        format = document.querySelector('#celcius').getAttribute('value');
+        format = 'metric'; 
     }
 
     else {
-        format = document.querySelector('#farenheit').getAttribute('value');
+        format = 'imperial';
     }
 
     // Your code here.
-    console.log("Format is " + format);
+    console.log("Format is: " + format);
 
     //set the query  
     let query;
     // Your code here.
 
+    // If it is an integer, that means the zip-code has been entered; so, it will have the query for zip-code
     if (Number.isInteger(location)) {
         query = "https://api.openweathermap.org/data/2.5/weather?zip=" + location  + "&appid=" + key + "&units=" + format;
     }
@@ -43,14 +44,14 @@ function gettingJSON(){
         query = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + key + "&units=" + format;
     }
 
-    console.log("Query is :" + query);
+    console.log("Query is: " + query);
 
     //Create and set variables for each of the elements you
     //need to update, location, temp, the image, etc.
 
-    let loc = document.querySelector("#loc");
-    let temp = document.querySelector("#temp");
-    let tempImg = document.querySelector("#tempImg");
+    let loc; 
+    let temp;
+    let tempImg;
     // Your code here.
 
 
@@ -59,15 +60,21 @@ function gettingJSON(){
         //elements in HTML.  
         //I would print the JSON to the console
         // Your code here.
+        
         console.log(JSON.stringify(json));
-            let text = json["weather"][0]["description"];
-            location.innerHTML = json.name;
-            temp.innerHTML = json["main"].temp +' with '+ text;
-            let link = "http://openweathermap.org/img/wn/" + json["weather"][0]["icon"] + ".png";
-            tempImg.setAttribute("src", link);
-            tempImg.setAttribute("alt", text);
-            tempImg.setAttribute("title", "Weather Image")
-            document.getElementById("forecast").style.display = "block";
+        loc = json['name'];
+        document.querySelector("#loc").innerHTML = loc;
+
+        temp = json['main']['temp']; 
+        document.querySelector("#temp").innerHTML = temp; 
+
+        let imageLink = "http://openweathermap.org/img/wn/" + json['weather'][0]['icon'] + '.png'; 
+        let altText = json['weather'][0]['description']; 
+        let imageTitle = "Weather"; 
+
+        document.getElementById("tempImg").setAttribute("alt", altText); 
+        document.getElementById("tempImg").setAttribute("src", imageLink); 
+        document.getElementById("tempImg").setAttribute("title", imageTitle); 
 
     });
 }
